48e7e4e4505fc5024a3a23c92cdaf535f0c9a703
Murilo Santori Boff <murilosantori45@gmail.com>
2023-07-26 10:50:08 -0300

    Creating Models and begining the front-end

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class ShortURL(models.Model):
+    original_url = models.URLField()
+    short_url = models.CharField(max_length=100)
+    time_date_created = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.original_url
