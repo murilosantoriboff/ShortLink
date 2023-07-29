0e791c41bea40ec857dcc68e3e99038bc56a9e68
Murilo Santori Boff <murilosantori45@gmail.com>
2023-07-29 09:32:34 -0300

    end

--- core/views.py
+++ core/views.py
@@ -15,5 +15,19 @@ def valida_link(request):
         messages.add_message(request, messages.constants.ERROR, 'Ja existe um link com esse nome!!')
         return redirect('/')
     
-    
+    link = ShortURL(original_url=original_url, short_url=short_url)
+
+    link.save()
+
+    messages.add_message(request, messages.constants.SUCCESS, 'Link criado com sucesso!!')
+    messages.add_message(request, messages.constants.INFO, 'Seu link é http://127.0.0.1:8000/'+short_url)
+    return redirect('/')
+
+def redirecionar(request, link):
+    url = ShortURL.objects.filter(short_url=link)
+    if len(url) == 0:
+        messages.add_message(request, messages.constants.ERROR, 'Ja não existe esse link')
+        return redirect('/')
+    else:
+        return redirect(url[0].original_url)
     
